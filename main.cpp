40ad1b837426e3dfdb17c66c5a8bc43bc13f55d8
mike <mango@live.cn>
2019-12-31 00:58:14 +0800

    added adaptive frame rate

--- main.cpp
+++ main.cpp
@@ -22,15 +22,29 @@ int main() {
 
     // connect
     asio::io_service io_service;
-    auto address = asio::ip::address_v4::from_string("169.254.89.26");
+    auto address = asio::ip::address_v4::from_string("192.168.1.107");
     asio::ip::tcp::socket pose_socket{io_service};
     asio::ip::tcp::socket frame_socket{io_service};
-    pose_socket.connect({address, 7766});
-    frame_socket.connect({address, 6677});
+
+    try {
+        pose_socket.connect({address, 23333});
+        frame_socket.connect({address, 24444});
+    } catch (const asio::system_error &error) {
+        std::cout << "Failed to connect: " << error.what() << std::endl;
+        exit(-1);
+    }
 
     // video for test
-    cv::VideoCapture camera{"data/test.mp4"};
-    cv::Mat raw_frame;
+    std::vector<cv::Mat> raw_frames;
+    for (cv::VideoCapture camera{"data/test.mp4"}; camera.grab(); ) {
+        raw_frames.emplace_back();
+        camera.retrieve(raw_frames.back());
+        std::cout << "Loaded frame #" << raw_frames.size() << std::endl;
+        if (raw_frames.size() >= 1500ul) {
+            break;
+        }
+    }
+
     cv::Mat eye_frame;
     cv::Mat frame;
 
@@ -41,6 +55,8 @@ int main() {
 
     auto frame_count = 0ul;
     auto last_time_point = std::chrono::high_resolution_clock::now();
+
+    auto t_start = 0.0;
 
     try {
         for (;;) {
@@ -54,36 +70,42 @@ int main() {
             static thread_local char json_string[4096ul];
             ZSTD_decompress(json_string, 4096, buffer, size);
 
-            std::string eye{rapidjson::Document{}.Parse(json_string)["eye"].GetString()};
+            rapidjson::Document json;
+            json.Parse(json_string);
+            std::string mode{json["mode"].GetString()};
+            auto time = json["time"].GetDouble();
+
+            if (t_start == 0.0) {
+                t_start = time;
+            }
+            auto frame_index = static_cast<uint32_t>((time - t_start) * 30.0);
+            if (frame_index >= raw_frames.size()) {
+                break;
+            }
 
             // read frame
-            camera >> raw_frame;
-            if (eye == "left") {
+            auto &&raw_frame = raw_frames[frame_index];
+            if (mode == "stereo") {
                 cv::resize(raw_frame, eye_frame, cv::Size2i{eye_frame_width, eye_frame_height});
                 eye_frame.copyTo(frame(cv::Rect{0, 0, eye_frame_width, eye_frame_height}));
-            } else if (eye == "right") {
-                cv::resize(raw_frame, eye_frame, cv::Size2i{eye_frame_width, eye_frame_height});
                 eye_frame.copyTo(frame(cv::Rect{eye_frame_width, 0, eye_frame_width, eye_frame_height}));
             } else {
                 cv::resize(raw_frame, frame, cv::Size2i{eye_frame_width * 2ul, eye_frame_height});
             }
 
-            if (eye != "left") {
+            // send frame
+            auto jpeg_size = 0ul;
+            tjCompress(jpeg_handle, frame.data, eye_frame_width * 2ul, 0, eye_frame_height, 3, frame_buffer.data() + 4ul, &jpeg_size, TJSAMP_444, 70, TJFLAG_FASTDCT);
+            *reinterpret_cast<std::array<uint8_t, 4ul> *>(frame_buffer.data()) = uint_to_u8vec4(jpeg_size);
+            asio::write(frame_socket, asio::buffer(frame_buffer.data(), jpeg_size + 4ul));
 
-                // send frame
-                auto jpeg_size = 0ul;
-                tjCompress(jpeg_handle, frame.data, eye_frame_width * 2ul, 0, eye_frame_height, 3, frame_buffer.data() + 4ul, &jpeg_size, TJSAMP_420, 75, TJFLAG_FASTDCT);
-                *reinterpret_cast<std::array<uint8_t, 4ul> *>(frame_buffer.data()) = uint_to_u8vec4(jpeg_size);
-                asio::write(frame_socket, asio::buffer(frame_buffer.data(), jpeg_size + 4ul));
-
-                if (++frame_count == 10ul) {
-                    using namespace std::chrono_literals;
-                    auto current_time_point = std::chrono::high_resolution_clock::now();
-                    auto seconds = (current_time_point - last_time_point) / 1ns * 1e-9;
-                    std::cout << "FPS: " << frame_count / seconds << std::endl;
-                    frame_count = 0ul;
-                    last_time_point = current_time_point;
-                }
+            if (++frame_count == 10ul) {
+                using namespace std::chrono_literals;
+                auto current_time_point = std::chrono::high_resolution_clock::now();
+                auto seconds = (current_time_point - last_time_point) / 1ns * 1e-9;
+                std::cout << "FPS: " << frame_count / seconds << std::endl;
+                frame_count = 0ul;
+                last_time_point = current_time_point;
             }
         }
     } catch (...) {}
